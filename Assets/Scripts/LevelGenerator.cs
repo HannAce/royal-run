6974e0d1bfb31446500c8e047bd2d72b16b5d520
Hannah Fisher <hannahtfisher@gmail.com>
2025-02-22 15:13:33 +0000

    Refactored LevelGenerator script and added MoveChunks
    Extracted previous code into their own methods, creating new method to move the starting chunks, and also added a path texture over the chunks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,32 +7,55 @@ public class LevelGenerator : MonoBehaviour
     
     [SerializeField] private int m_startingChunkAmount;
     [SerializeField] private float m_chunkLength;
+    [SerializeField] private float m_moveSpeed;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private GameObject[] m_chunks = new GameObject[12];
+    
     void Start()
+    {
+        SpawnChunks();
+    }
+
+    void Update()
+    {
+        MoveChunks();
+    }
+
+    private void SpawnChunks()
     {
         for (int i = 0; i < m_startingChunkAmount; i++)
         {
-            float spawnPosZ;
-            
-            if (i == 0)
-            {
-                spawnPosZ = transform.position.z;
-            }
-            else
-            {
-                spawnPosZ = transform.position.z + (i * m_chunkLength);
-            }
+            float spawnPosZ = CalculateSpawnPosZ(i);
 
             Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPosZ);
 
-            Instantiate(m_chunkPrefab, chunkSpawnPos, Quaternion.identity, m_chunkContainer);
+            GameObject newChunk = Instantiate(m_chunkPrefab, chunkSpawnPos, Quaternion.identity, m_chunkContainer);
+            
+            m_chunks[i] = newChunk;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private float CalculateSpawnPosZ(int i)
     {
+        float spawnPosZ;
 
+        if (i == 0)
+        {
+            spawnPosZ = transform.position.z;
+        }
+        else
+        {
+            spawnPosZ = transform.position.z + (i * m_chunkLength);
+        }
+
+        return spawnPosZ;
+    }
+
+    private void MoveChunks()
+    {
+        for (int i = 0; i < m_chunks.Length; i++)
+        {
+            m_chunks[i].transform.Translate(0, 0, -1 * m_moveSpeed * Time.deltaTime);
+        }
     }
 }
