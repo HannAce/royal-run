d2ee7eae6c4414b0364e0deebfd05f28e5d02579
Hannah Fisher <hannahtfisher@gmail.com>
2025-03-02 17:33:40 +0000

    Added walls to path and clamped movement
    Added walls to the chunkPrefab (and updated collider and scale), and clamped player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float m_moveSpeed;
     private Rigidbody m_rb;
-    private Vector3 m_movement;
+    private Vector3 m_movementInput;
 
     private void Awake()
     {
@@ -20,15 +20,23 @@ public class PlayerController : MonoBehaviour
     
     public void Move(InputAction.CallbackContext context)
     {
-        m_movement = context.ReadValue<Vector2>();
+        m_movementInput = context.ReadValue<Vector2>();
     }
     
     private void HandleMovement()
     {
         Vector3 currentPos = m_rb.position;
-        Vector3 moveDirection = new Vector3(m_movement.x, 0, m_movement.y);
+        Vector3 moveDirection = new Vector3(m_movementInput.x, 0, m_movementInput.y);
         Vector3 newPos = currentPos + moveDirection * (m_moveSpeed * Time.fixedDeltaTime);
         
-        m_rb.MovePosition(newPos);
+        float clampedX = Mathf.Clamp(newPos.x, -4f, 4f);
+        float clampedZ = Mathf.Clamp(newPos.z, -1.5f, 3f);
+        Vector3 clampedPos = new Vector3(clampedX, newPos.y, clampedZ);
+        
+        m_rb.MovePosition(clampedPos);
+    }
+
+    private void ClampMovement()
+    {
     }
 }
